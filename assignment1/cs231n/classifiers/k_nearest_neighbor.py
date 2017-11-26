9be7d298709235750a2e10f446fdcdf378e7fe24
Hank Ehly <henry.ehly@gmail.com>
2017-11-27 08:27:02 +0900

    Use L2 distance function

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -73,7 +73,7 @@ class KNearestNeighbor(object):
                 # training point, and store the result in dists[i, j]. You should   #
                 # not use a loop over dimension.                                    #
                 #####################################################################
-                dists[i, j] = np.dot(X[i], self.X_train[j])
+                dists[i, j] = np.sqrt(np.sum(np.square(self.X_train[j] - X[i])))
                 #####################################################################
                 #                       END OF YOUR CODE                            #
                 #####################################################################
